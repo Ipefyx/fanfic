15d1e95969f826390f4b74835e4b15f4c13ee7b4
Ipefyx <kernel.panic@hotmail.be>
2012-04-10 20:01:10 +0200

    Base commit

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+RAILS_GEM_VERSION = '3.2.1'
+
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Fanfic::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fanfic::Application.config.secret_token = '92c9e048fcc455e8d585a90a68261ced816f7a6a22b450487c143a67d1ac703d11ba6bb129f81335241d09c3525d968d39e8645bfe915cdbe3936542daae1108'
